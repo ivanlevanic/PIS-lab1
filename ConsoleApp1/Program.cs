2ef8f4ca7e830d14df96864c99e0a10912917daf
Petra2121 <62608052+Petra2121@users.noreply.github.com>
2020-03-27 20:22:09 +0100

    Update Program.cs
    Dodan tekst.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Moje ime je medvjedić Lino.");
         }
     }
 }
